dca2709273ac51b54e4ccc22f1310056497d3b6b
itissalma <itissalma@aucegypt.edu>
2023-12-16 15:28:19 +0200

    fixed routing

--- src/App.js
+++ src/App.js
@@ -1,21 +1,40 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import LoginForm from './LoginForm/login';
 import Dashboard from './Dashboard/dashboard';
 import './App.css';
-import Questionnare from './Questionnaire/questions';
+import Questionnaire from './Questionnaire/questions';
 import BusinessPlan from './BusinessPlan/document';
 
 function App() {
+  const isAuthenticated = !!sessionStorage.getItem('username');
+
+  const PrivateRoute = ({ element, ...props }) => {
+    if (!isAuthenticated) {
+      return <Navigate to="/login" replace />;
+    }
+
+    return element;
+  };
+
   return (
     <Router>
       <div className="App">
         <header className="App-header">
           <Routes>
             <Route path="/login" element={<LoginForm />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/questions" element={<Questionnare />} />
-            <Route path="/document/:id" element={<BusinessPlan />} />
+            <Route
+              path="/dashboard"
+              element={<PrivateRoute element={<Dashboard />} />}
+            />
+            <Route
+              path="/questions"
+              element={<PrivateRoute element={<Questionnaire />} />}
+            />
+            <Route
+              path="/document/:id"
+              element={<PrivateRoute element={<BusinessPlan />} />}
+            />
             <Route path="/" element={<LoginForm />} />
           </Routes>
         </header>
